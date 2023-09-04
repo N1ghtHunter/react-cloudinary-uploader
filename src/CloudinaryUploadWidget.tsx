5ac575ec598f4bd2af15f0c5dedd4dd07b685e52
Mazin Islam <93228608+N1ghtHunter@users.noreply.github.com>
2023-09-04 04:08:14 +0300

    docs: added contributing.md file

--- src/CloudinaryUploadWidget.tsx
+++ src/CloudinaryUploadWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react';
 
 // i want accept style and className props
 interface CloudinaryUploadWidgetProps {
@@ -24,66 +24,66 @@ const CloudinaryUploadWidget: React.FC<CloudinaryUploadWidgetProps> = ({
   buttonClass,
   children,
 }) => {
-   const cloudinaryWidget = useRef<any | null>(null);
+  const cloudinaryWidget = useRef<any | null>(null);
   const widgetRef = useRef<any | null>(null);
   useEffect(() => {
     // check if there is a script tag with the cloudinary url
     const loadedScript = document.querySelector(`script[src="https://upload-widget.cloudinary.com/global/all.js"]`);
     if (loadedScript) {
-      cloudinaryWidget.current = (window as any).cloudinary
-      widgetRef.current= cloudinaryWidget.current.createUploadWidget(
+      cloudinaryWidget.current = (window as any).cloudinary;
+      widgetRef.current = cloudinaryWidget.current.createUploadWidget(
         {
           cloudName,
           uploadPreset,
           ...options,
         },
         (error: any, result: any) => {
-          if (!error && result && result.event === "success") {
+          if (!error && result && result.event === 'success') {
             if (onUploadSuccess) {
               onUploadSuccess(result.info);
-            }else{
+            } else {
               console.log(result.info);
             }
-            if(error){
-              if(onUploadFailure){
+            if (error) {
+              if (onUploadFailure) {
                 onUploadFailure(error);
-              }else{
+              } else {
                 console.log(error);
               }
             }
           }
-        }
+        },
       );
       return;
     }
-    const script = document.createElement("script");
-    script.src = "https://upload-widget.cloudinary.com/global/all.js";
+    const script = document.createElement('script');
+    script.src = 'https://upload-widget.cloudinary.com/global/all.js';
     script.async = true;
-    script.type = "text/javascript";
+    script.type = 'text/javascript';
     script.onload = () => {
-      cloudinaryWidget.current = (window as any).cloudinary
-      widgetRef.current= cloudinaryWidget.current.createUploadWidget(
+      cloudinaryWidget.current = (window as any).cloudinary;
+      widgetRef.current = cloudinaryWidget.current.createUploadWidget(
         {
           cloudName,
           uploadPreset,
           ...options,
         },
         (error: any, result: any) => {
-          if (!error && result && result.event === "success") {
+          if (!error && result && result.event === 'success') {
             if (onUploadSuccess) {
               onUploadSuccess(result.info);
-            }else{
+            } else {
               console.log(result.info);
             }
-            if(error){
-              if(onUploadFailure){
+            if (error) {
+              if (onUploadFailure) {
                 onUploadFailure(error);
-              }else{
+              } else {
                 console.log(error);
               }
             }
           }
-        }
+        },
       );
     };
     document.body.appendChild(script);
@@ -92,20 +92,16 @@ const CloudinaryUploadWidget: React.FC<CloudinaryUploadWidgetProps> = ({
       document.body.removeChild(script);
     };
   }, [cloudName, uploadPreset, options, onUploadSuccess]);
-  
-    if (children) {
+
+  if (children) {
     return React.cloneElement(children, {
-      onClick: () => widgetRef.current.open()
+      onClick: () => widgetRef.current.open(),
     });
   }
 
   return (
-    <button
-      onClick={() => widgetRef.current.open()}
-      style={{ border: "none", ...buttonStyle }}
-      className={buttonClass}
-    >
-      {buttonText || "Upload Image"}
+    <button onClick={() => widgetRef.current.open()} style={{ border: 'none', ...buttonStyle }} className={buttonClass}>
+      {buttonText || 'Upload Image'}
     </button>
   );
 };
